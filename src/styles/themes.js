5013f5faba0910989f750fc48d8cdc4a440d5f2e
jennifertorres <jntorres1@espe.edu.ec>
2025-07-15 11:51:57 -0500

    feat: cuarta versión examen lit

---
+++ src/styles/themes.js
@@ -0,0 +1,23 @@
+// src/styles/themes.js
+export const themes = {
+  azul: {
+    borderColor: '#1e3a8a',
+    headerBg: '#3b82f6',
+  },
+  verde: {
+    borderColor: '#166534',
+    headerBg: '#22c55e',
+  },
+  rojo: {
+    borderColor: '#991b1b',
+    headerBg: '#ef4444',
+  },
+  gris: {
+    borderColor: '#374151',
+    headerBg: '#9ca3af',
+  },
+  naranja: {
+    borderColor: '#92400e',
+    headerBg: '#f97316',
+  },
+};
